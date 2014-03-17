ca37fd1ecad8779c15f5d8c0384b247b618b2790
gioadami <giordano.adami@gmail.com>
2014-03-17 10:34:57 +0100

    Sync by type, no user.

--- src/main/java/eu/trentorise/smartcampus/vas/communicator/controller/ObjectController.java
+++ src/main/java/eu/trentorise/smartcampus/vas/communicator/controller/ObjectController.java
@@ -17,6 +17,7 @@ package eu.trentorise.smartcampus.vas.communicator.controller;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.TreeMap;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -58,5 +59,28 @@ public class ObjectController extends RestController {
 		SyncData out = communicatorManager.synchronize(user, syncReq.getSyncData());
 		return new ResponseEntity<SyncData>(out,HttpStatus.OK);
 	}
+	
+	@RequestMapping(method = RequestMethod.POST, value = "/synctype")
+	public @ResponseBody
+	ResponseEntity<SyncData> syncData(HttpServletRequest request, HttpServletResponse response, @RequestParam long since, @RequestParam String type) throws DataException, IOException, NotFoundException, ClassNotFoundException, SecurityException, ProfileServiceException {
+		SyncDataRequest syncReq = convertTypeRequest(type, since);
+		SyncData out = communicatorManager.simpleSynchronize(syncReq.getSyncData());
+		return new ResponseEntity<SyncData>(out,HttpStatus.OK);
+	}	
 
+	public static SyncDataRequest convertTypeRequest(String sourceType, long since) throws ClassNotFoundException {
+		SyncData data = new SyncData();
+		try {
+			data.setVersion(since);
+		} catch (Exception e1) {
+			data.setVersion(0);
+		}
+		Map<String, Object> include = new TreeMap<String, Object>();
+		include.put("type", sourceType);
+
+		data.setInclude(include);
+
+		return new SyncDataRequest(data, since);
+	}
+	
 }
